f38f2d584999f142a0a10f5cb5b38e06719461f8
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 20:03:56 +0300

    EmployeeCardsVM.cs

--- VertexHRMS.BLL/ModelVM/Employees/EmployeeCardsVM.cs
+++ VertexHRMS.BLL/ModelVM/Employees/EmployeeCardsVM.cs
@@ -1,10 +1,9 @@
-﻿
-namespace VertexHRMS.BLL.ModelVM.Employees
-{
-	public class EmployeeCardsVM
-	{
-		public List<GetAllUserByDepartmentIdVM> Employees { get; set; } = new();
-		public string? Message { get; set; }
-		public bool HasError { get; set; }
-	}
-}
+﻿namespace VertexHRMS.BLL.ModelVM.Employees
+{ 
+	 public class EmployeeCardsVM
+	 {
+		 public List<GetAllUserByDepartmentIdVM> Employees { get; set; } = new();
+		 public string? Message { get; set; }
+		 public bool HasError { get; set; }
+	 } 
+} 
