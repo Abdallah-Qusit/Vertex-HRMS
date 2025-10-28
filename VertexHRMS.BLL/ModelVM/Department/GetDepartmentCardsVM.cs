dea434c8ca9678f14446eef395f2226e0e941204
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 20:03:13 +0300

    GetDepartmentCardsVM.cs

--- VertexHRMS.BLL/ModelVM/Department/GetDepartmentCardsVM.cs
+++ VertexHRMS.BLL/ModelVM/Department/GetDepartmentCardsVM.cs
@@ -1,10 +1,10 @@
-﻿namespace VertexHRMS.BLL.ModelVM.Department
-{
-    public class GetDepartmentCardsVM
-    {
-        public int DepartmentId { get; set; }
-        public string DepartmentName { get; set; }
-        public string Description { get; set; }  
-        public List<GetAllUserByDepartmentIdVM> Employees { get; set; } = new();
-    }
-}
+ ﻿namespace VertexHRMS.BLL.ModelVM.Department
+{ 
+     public class GetDepartmentCardsVM
+     {
+         public int DepartmentId { get; set; }
+         public string DepartmentName { get; set; }
+         public string Description { get; set; }  
+         public List<GetAllUserByDepartmentIdVM> Employees { get; set; } = new();
+     } 
+} 
