9eca950308eef5d0ed8ab35616d54b6b5433a41d
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 19:53:13 +0300

    IDepartmentCardsRepo.cs

--- VertexHRMS.DAL/Repo/Abstraction/IDepartmentCardsRepo.cs
+++ VertexHRMS.DAL/Repo/Abstraction/IDepartmentCardsRepo.cs
@@ -1,14 +1,13 @@
-﻿
-namespace VertexHRMS.DAL.Repo.Abstraction
-{
-    public interface IDepartmentCardsRepo
+﻿namespace VertexHRMS.DAL.Repo.Abstraction
+{ 
+    public  interface IDepartmentCardsRepo
     {
-        bool Create(Department department);
-        bool Delete(int departmentId);
-        Department? GetById(int departmentId);
-        List<Department> GetDeparts(Expression<Func<Department, bool>>? filter = null);
-        Task<List<Department>> GetAllWithEmployeeAsync();
-        Task<Department?> GetByIdWithEmployeeAsync(int id);
-        Task<List<Department>> SearchDepartmentsAsync(string name);
+        bool  Create(Department department);
+        bool  Delete(int departmentId);
+        Department?  GetById(int departmentId);
+        List<Department>  GetDeparts(Expression<Func<Department, bool>>? filter = null);
+        Task<List<Department>>  GetAllWithEmployeeAsync();
+        Task<Department?>  GetByIdWithEmployeeAsync(int id);
+        Task<List<Department>>  SearchDepartmentsAsync(string name);
     }
 }
