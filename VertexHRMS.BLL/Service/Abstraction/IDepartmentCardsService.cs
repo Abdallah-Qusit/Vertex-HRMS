89a7d6fbcad96a6fe9a98c9f0a2762694d09aed4
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 19:54:42 +0300

    IDepartmentCardsService.cs

--- VertexHRMS.BLL/Service/Abstraction/IDepartmentCardsService.cs
+++ VertexHRMS.BLL/Service/Abstraction/IDepartmentCardsService.cs
@@ -1,12 +1,11 @@
-﻿namespace VertexHRMS.BLL.Service.Abstraction
+﻿namespace  VertexHRMS.BLL.Service.Abstraction
 {
-    public interface IDepartmentCardsService
-    {
-        (bool, string) Create(CreateDepartmentCardsVM depart);
-        (bool, string, List<GetDepartmentCardsVM>) Getdepart();
-
-        Task<List<GetDepartmentCardsVM>> GetAllDepartmentsAsync();
-        Task<GetDepartmentCardsVM> GetDepartmentByIdAsync(int id);
-        Task<List<GetDepartmentCardsVM>> SearchDepartmentsAsync(string name);
-    }
-}
+    public  interface IDepartmentCardsService
+    { 
+        (bool, string)  Create(CreateDepartmentCardsVM depart);
+        (bool, string,  List<GetDepartmentCardsVM>) Getdepart();
+        Task<List<GetDepartmentCardsVM>>  GetAllDepartmentsAsync();
+        Task<GetDepartmentCardsVM>  GetDepartmentByIdAsync(int id);
+        Task<List<GetDepartmentCardsVM>>  SearchDepartmentsAsync(string name);
+    } 
+} 
