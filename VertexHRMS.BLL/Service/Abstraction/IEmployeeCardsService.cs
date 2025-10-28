e05927cd7f146990f317408ec696e8726963e56a
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 19:55:21 +0300

    IEmployeeCardsService.cs

--- VertexHRMS.BLL/Service/Abstraction/IEmployeeCardsService.cs
+++ VertexHRMS.BLL/Service/Abstraction/IEmployeeCardsService.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-namespace VertexHRMS.BLL.Service.Abstraction
+﻿using  System;
+using  System.Collections.Generic;
+namespace  VertexHRMS.BLL.Service.Abstraction
 {
-    public interface IEmployeeCardsService
-    {
-        Task<EmployeeCardsVM?> GetByDepartmentId(int departmentId);
-    }
-}
+    public interface  IEmployeeCardsService
+    { 
+        Task<EmployeeCardsVM?>  GetByDepartmentId(int departmentId);
+    } 
+} 
