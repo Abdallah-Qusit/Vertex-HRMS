39f07dd19a05d7d397e917bd1655932c0ceb55ec
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 20:05:11 +0300

    GetAllUserByDepartmentIdVM.cs

--- VertexHRMS.BLL/ModelVM/Employees/GetAllUserByDepartmentIdVM.cs
+++ VertexHRMS.BLL/ModelVM/Employees/GetAllUserByDepartmentIdVM.cs
@@ -1,15 +1,12 @@
-﻿
-namespace VertexHRMS.BLL.ModelVM.Employees
-{
-    public class GetAllUserByDepartmentIdVM
-    {
-        public int EmployeeId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string ImagePath { get; set; }
-        public int DepartmentId { get; set; }
+﻿namespace VertexHRMS.BLL.ModelVM.Employees
+{ 
+     public class GetAllUserByDepartmentIdVM
+     {
+         public int EmployeeId { get; set; }
+         public string FirstName { get; set; }
+         public string LastName { get; set; }
+         public string ImagePath { get; set; }
+         public int DepartmentId { get; set; }
         
-       
-
-    }
-}
+     }
+} 
