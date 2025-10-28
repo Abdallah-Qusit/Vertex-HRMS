d7df849322d7cf0352cfa9662d5cd671ee823c7a
Zyad Al-Abiad <163039828+Zyadateff@users.noreply.github.com>
2025-10-28 20:02:47 +0300

    CreateDepartmentCardsVM.cs

--- VertexHRMS.BLL/ModelVM/Department/CreateDepartmentCardsVM.cs
+++ VertexHRMS.BLL/ModelVM/Department/CreateDepartmentCardsVM.cs
@@ -1,11 +1,12 @@
-﻿namespace VertexHRMS.BLL.ModelVM.Department
-{
-    public class CreateDepartmentCardsVM
-    {
-        [Required(ErrorMessage = "Name is required :(")]
-        [MinLength(2, ErrorMessage = "Plz Enter Length bigger Than 2 :(")]
-        public string DepartmentName { get; set; }   
 
-        public string Description { get; set; }      
-    }
-}
+namespace VertexHRMS.BLL.ModelVM.Department
+{ 
+    public class  CreateDepartmentCardsVM
+    { 
+         [Required(ErrorMessage = "Name is required :(")]
+         [MinLength(2, ErrorMessage = "Plz Enter Length bigger Than 2 :(")]
+         public string DepartmentName { get; set; }   
+ 
+         public string Description { get; set; }      
+    } 
+} 
